5f54edb373e106eee1e265ba04ab0f086629682a
DmitriyKudrya <dimka2005@mail.ru>
2018-07-20 15:53:14 +0300

    corr files

--- main.js
+++ main.js
@@ -1,46 +1,43 @@
-function validate() {
 
-  var x = document.getElementById("name").value;
-  var y = document.getElementById("job").value;
-
-  if (x.length==0) {
-    alert("Введите свое имя");
-    return false;
-  }
-
-  if (y.length==0) {
-    alert("Введите свой вид деятельности");
-    return false;
-  }
-}
-   // at=y.indexOf("@");
-   // dot=y.indexOf(".");
- 
-   // if (at<1 || dot <1){
-   //    alert("Email введен не верно");
-   //    return false;
-   // }
-
-var a = document.getElementById("name").value;
-var b = document.getElementById("job").value;
+window.onload = function() {
+  document.querySelector("#sendData").onclick = function() {
     
-var xhr = new XMLHttpRequest();
-    
-xhr.open("POST", "https://reqres.in/api/users", true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-xhr.send("name=" + encodeURIComponent(a) + "&job=" + encodeURIComponent(b)); 
+    var x = document.getElementById("name").value;
+    var y = document.getElementById("job").value;
 
-xhr.onreadystatechange = function() {
-  if(xhr.readyState == 4) {
-    if(xmlhttp.status == 201) {
-      response ();
+    if (x.length==0) {
+      alert("Введите свое имя");
+      return false;
     }
-  } 
+
+    if (y.length==0) {
+      alert("Введите свой вид деятельности");
+      return false;
+    } 
+    ajaxSend();
+    alert ("Данные успешно отправлены");
+  }
 }
 
-function response () {
-  
-}
+function ajaxSend() { 
+
+  var a = document.getElementById("name").value;
+  var b = document.getElementById("job").value;
+        
+  var xhr = new XMLHttpRequest();
+        
+  xhr.open("POST", "https://reqres.in/api/users", true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.send("name=" + encodeURIComponent(a) + "&job=" + encodeURIComponent(b)); 
+
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4 && xhr.status == 201) { 
+        document.querySelector("#result").innerHTML = xhr.responseText;
+      }
+    } 
+  }
+
+
 
 
 
