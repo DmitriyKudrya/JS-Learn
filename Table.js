c990d0cee7b5f4159f75a6fff840e603134ddb78
DmitriyKudrya <dimka2005@mail.ru>
2018-07-25 12:08:10 +0300

    corr files

--- Table.js
+++ Table.js
@@ -1,11 +1,16 @@
-var n3 = localStorage.getItem("n2");
-var j3 = localStorage.getItem("j2");
-var id3 = localStorage.getItem("id2");
-var cr3 = localStorage.getItem("cr2");
 
-document.getElementById("n1").innerHTML = n3.value;
-document.getElementById("j1").innerHTML = j3.value;
-document.getElementById("id1").innerHTML = id3.value;
-document.getElementById("cr1").innerHTML = cr3.value;
+window.onload = function() {
 
-localStorage.clear();
+  var n3 = localStorage.getItem("n2");
+  var j3 = localStorage.getItem("j2");
+  var id3 = localStorage.getItem("id2");
+  var cr3 = localStorage.getItem("cr2");
+
+  document.getElementById("n1").innerHTML = n3;
+  document.getElementById("j1").innerHTML = j3;
+  document.getElementById("id1").innerHTML = id3;
+  document.getElementById("cr1").innerHTML = cr3;
+
+  localStorage.clear();
+
+}
--- main.js
+++ main.js
@@ -52,19 +52,3 @@ function ajaxSend() {
   } 
 }
 
-function createData() {
-
-  var n3 = localStorage.getItem("n2");
-  var j3 = localStorage.getItem("j2");
-  var id3 = localStorage.getItem("id2");
-  var cr3 = localStorage.getItem("cr2");
-
-  document.getElementById("n1").innerHTML = n3.value;
-  document.getElementById("j1").innerHTML = j3.value;
-  document.getElementById("id1").innerHTML = id3.value;
-  document.getElementById("cr1").innerHTML = cr3.value;
-
-  localStorage.clear();
-
-}
-
